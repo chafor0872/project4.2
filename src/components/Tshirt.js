de3ae78230e48a10e694a524f7c33bc301804e2a
Chase Forbes <chafor0872@students.ecpi.edu>
2022-12-11 16:40:14 -0500

    update

--- src/components/Tshirt.js
+++ src/components/Tshirt.js
@@ -8,7 +8,13 @@ const Tshirt = () => (
         <section className="grid-item">
         <div className="grid-item">
             <img src="https://www.bing.com/ck/a?!&&p=9910992282bb947eJmltdHM9MTY3MDExMjAwMCZpZ3VpZD0zNTIzOGRhYi1jY2FlLTZjM2MtMDkxOS05ZmM5Y2RhYzZkOWMmaW5zaWQ9NTQ1OQ&ptn=3&hsh=3&fclid=35238dab-ccae-6c3c-0919-9fc9cdac6d9c&u=a1L3Nob3AvcHJvZHVjdHBhZ2U_cT1iYXNpYyt0K3NoaXJ0JmZpbHRlcnM9c2NlbmFyaW86IjE3IiBnVHlwZToiMTIiIGdJZDoiMzE0NDU3MzY2MzkiIGdHbG9iYWxPZmZlcklkczoiMzE0NDU3MzY2MzkiIFByb2R1Y3RQYWdlS2V5QXBwZW5kZXI6InNlcnBzaG9wYW5zIiZwcm9kdWN0cGFnZT10cnVlJkZPUk09U1NBUEMx&ntb=1" />
-            <h3>Basic Tee</h3>
+            <h3>Basic White Tee</h3>
+            <img src="https://www.google.com/aclk?sa=l&ai=DChcSEwiWsK3ywvL7AhVFy8gKHaYND8sYABADGgJxdQ&sig=AOD64_1HRe5jFiGbm0LQBOz1uOhsfNh7dQ&adurl&ctype=5&ved=2ahUKEwiK4aPywvL7AhV3qnIEHX01DoQQvhd6BAgBEHQ" />
+            <h3>Basic Black Tee</h3>
+            <img src="https://www.google.com/aclk?sa=l&ai=DChcSEwiWsK3ywvL7AhVFy8gKHaYND8sYABAJGgJxdQ&sig=AOD64_0ZJ-z8TuyMYTUpkYuwbGiK3Ipq7g&adurl&ctype=5&ved=2ahUKEwiK4aPywvL7AhV3qnIEHX01DoQQvhd6BQgBEIcB" />
+            <h3>Basic Red Tee</h3>
+            <img src="https://www.google.com/aclk?sa=l&ai=DChcSEwiWsK3ywvL7AhVFy8gKHaYND8sYABANGgJxdQ&sig=AOD64_2x548WamD68LVckxF9FZhGStiX6w&adurl&ctype=5&ved=2ahUKEwiK4aPywvL7AhV3qnIEHX01DoQQvhd6BQgBEJYB" />
+            <h3>Basic Grey Tee</h3>
             </div>
             </section>
     </div>
